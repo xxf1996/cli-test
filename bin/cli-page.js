ecf8d516877c2c1866bc2e78504edaa62575d5e6
xiexuefeng <1158231926@qq.com>
2019-03-26 17:55:01 +0800

    add readme

--- bin/cli-page.js
+++ bin/cli-page.js
@@ -41,14 +41,44 @@ if (glob.sync(path.join('.', 'vue.config.js')).length) {
       message: '页面标题：',
       default: pageName
     }
-  ]).then(answer => {
+  ]).then(answer => { // 复制页面模板文件
     const pageTemp = path.join(__dirname, '.download-temp', 'page_template')
     const pageDir = path.join('src', 'pages', pageName)
     pageConfig = Object.assign({}, answer, { pageName, pageDir })
     return fse.copy(path.join(pageTemp, answer.pageType), pageDir)
-  }).then(res => {
-    console.log(pageConfig)
+  }).then(res => { // 渲染页面的模板
     return new Metal(pageConfig.pageDir)
+      .clean(false)
+      .source('.')
+      .destination('.')
+      .use((files, metalsmith, done) => {
+        Object.keys(files).forEach(name => {
+          let file = files[name]
+          file.contents = Buffer.from(ejs.render(file.contents.toString(), pageConfig))
+        })
+        done()
+      })
+      .build(err => {
+        return err? Promise.reject(err): true
+      })
+  }).then(() => { // 复制路由模板文件至相应路径
+    const routeTemp = path.join(__dirname, '.download-temp', 'route_template')
+    let res
+    let routePath = pageConfig.pageRoute.split('/')
+    const routeDir = path.join('src', 'router', routePath[1])
+    if (routePath.length === 2) {
+      res = fse.copy(path.join(routeTemp, 'haveChild.js'), path.join(routeDir, 'index.js'))
+    } else if (routePath.length === 3) {
+      res = fse.copy(path.join(routeTemp, 'noChild.js'), path.join(routeDir, routePath[2] + '.js'))
+    }
+    Object.assign(pageConfig, {
+      routeDir,
+      pageSrc: `@/pages/${pageConfig.pageName}/Index` // 对应页面组件路径
+    })
+    return res
+  }).then(res => { // 对路由模板文件进行渲染
+    console.log(pageConfig)
+    return new Metal(pageConfig.routeDir)
       .clean(false)
       .source('.')
       .destination('.')
